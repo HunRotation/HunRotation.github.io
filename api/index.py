cbf999919a1c3af078c4c37d8f559a9f26e713a9
HunRotation <Huny8708@gmail.com>
2025-10-13 21:27:50 +0900

    resolve the CORS issue

--- api/index.py
+++ api/index.py
@@ -13,9 +13,13 @@ load_dotenv(dotenv_path=dotenv_path)
 app = FastAPI()
 
 # Configure CORS
+origins = [
+    "https://hunrotation.github.io",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
